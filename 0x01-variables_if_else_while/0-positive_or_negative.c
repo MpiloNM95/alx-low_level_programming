7a2b9e5c101667a0e37bc78b1f40daa7268b9fcf
MpiloNM95 <mpilonmafu95@gmail.com>
2022-07-01 05:46:16 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
- * and print last digit of the number stored in variable n
- * Return: 0 Always (Success), non zero otherwise (Unsuccessful)
+ *main - entry point
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success), non zero otherwise (Unsuccessful)
  */
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-if (n == 0)
-printf("%d is zero\n", n);
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	printf("%d is positive\n", n);
+	if (n == 0)
+	printf("%d is zero\n", n);
+	if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
 }

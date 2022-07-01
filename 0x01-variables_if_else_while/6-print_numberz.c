c3aee0f482fc6f5c48d880dccd540356f52925a5
MpiloNM95 <mpilonmafu95@gmail.com>
2022-07-01 09:56:43 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ * and prints the last digit of the number stored in variable n
+ * Return: 0 Always (success)
+ */
+int main(void)
+{
+	int ch;
+	
+	for (ch = 48;  ch <= 57; ch++)
+	{
+	putchar(ch);
+	}
+	putchar(10); /* this ascii code for a new line*/
+	return (0);
+}

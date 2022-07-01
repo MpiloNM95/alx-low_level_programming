f9b111a7c5d76aefcbbb762d40b85eb5fad895c1
MpiloNM95 <mpilonmafu95@gmail.com>
2022-07-01 07:44:18 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * and print the last digit of the number in variable n
+ * Return: 0 Always (Success), non zero otherwise (Unsuccessful)
+ */
+int main(void)
+{
+	int ch;
+
+	for(ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}

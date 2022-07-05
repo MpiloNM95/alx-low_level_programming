e39004c316c90009e17a5ff342ba798da708d198
MpiloNM95 <mpilonmafu95@gmail.com>
2022-07-05 04:04:15 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,10 @@
 
 /**
  * _islower - returns 1 if a is lowercase, 0 otherwise
- * @a: interger to be tested
+ * @c: interger to be tested
  * Return: 1 or 0
  */
-int _islower(int a)
+int _islower(int c)
 {
-return (a >= 97 && a <= 122);
+return (c >= 'a' && c <= 'z');
 }

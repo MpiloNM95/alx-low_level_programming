2e59547d96a50d2f2aa588658144109ac85bd355
MpiloNM95 <mpilonmafu95@gmail.com>
2022-07-07 08:57:13 -0700

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 with the exclusion of 2 and 4.
+ */
+void print_most_numbers(void)
+{
+int x;
+
+for (x = 0; x < 10; x++)
+{
+if (x != 2 && x != 4)
+_putchar(x + '0');
+}
+}

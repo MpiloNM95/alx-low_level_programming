2f4ae6df1643c38ce7fcab13f9b4d460498b424e
MpiloNM95 <mpilonmafu95@gmail.com>
2022-07-11 05:46:17 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,21 @@
  */
 void rev_string(char *s)
 {
+char tmp;
+int i, length1, length2;
 
-int i = 0;
-while (s[i])
-i++;
+length1 = 0;
+length2 = 0;
 
-while (i--)
+while (s[length1] != '\0')
+length1++;
+
+length2 = length1 - 1;
+for (i = 0; i < length1 / 2; i++)
 {
-_putchar(s[i]);
+tmp = s[i];
+s[i] = s[length2];
+s[length2] = tmp;
+length2 -= 1;
 }
-_putchar('\n');
-
 }

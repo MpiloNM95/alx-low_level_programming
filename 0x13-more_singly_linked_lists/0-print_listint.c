00e30cccfb134fa63a94dfcf2d507bc8ae8cdd22
MpiloNM95 <mpilonmafu95@gmail.com>
2022-08-15 05:41:27 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,17 +8,18 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+    size_t i = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
-
-	return (count);
+    while(h)
+    {
+        if(h == NULL)
+            return (1);
+        else
+        {
+            printf("%d", h->n);
+            i++;
+            h = h->next;
+        }
+    }
+    return (i);
 }

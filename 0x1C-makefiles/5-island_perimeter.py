dbdf209896c1ed98825d50511642c324adfa553c
MpiloNM95 <mpilonmafu95@gmail.com>
2022-12-07 03:50:47 -0800

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,3 +4,18 @@
 
 def island_perimeter(grid):
     """Returns perimeter of the island"""
+    c = 0
+    length = len(grid) -1
+    width = len(grid[0]) -1
+
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if == length or grid[i + 1][j] != 1:
+                    c += 1
